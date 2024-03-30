d8512a399c54c7b71687bb0cba16662dd48ce1b7
dani maccari <dperezmaccari@gmail.com>
2024-03-30 01:22:30 +0100

    host join mechanic

--- Museum-Client/Scripts/Menu_Test.gd
+++ Museum-Client/Scripts/Menu_Test.gd
@@ -1,10 +1,30 @@
 extends Control
 
+@export var ADDRESS = "127.0.0.1"
+@export var PORT = 8080
+var max_clients = 10
+var peer
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	multiplayer.peer_connected.connect(PlayerJustConnected)
+	multiplayer.peer_disconnected.connect(PlayerJustDisconnected)
+	multiplayer.connected_to_server.connect(ConnectedToServer)
+	multiplayer.connection_failed.connect(ConnectionFailed)
 	pass # Replace with function body.
 
+# called on both sides
+func PlayerJustConnected(id):
+	print("player connected ", id)
+
+func PlayerJustDisconnected(id):
+	print("player disconnected", id)
+
+# called only in client side
+func ConnectedToServer():
+	print("connected to SERVER")
+
+func ConnectionFailed():
+	print("connection FAILED")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,12 +32,36 @@ func _process(delta):
 
 
 func _on_host_pressed():
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(PORT, max_clients)
+	if error != OK:
+		print("coulndt host: ", error)
+		
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	
+	multiplayer.set_multiplayer_peer(peer)
+	print("waiting for players")
+	
 	pass # Replace with function body.
 
 
 func _on_join_pressed():
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(ADDRESS, PORT)
+	
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	
+	multiplayer.set_multiplayer_peer(peer)
 	pass # Replace with function body.
 
 
 func _on_start_pressed():
+	StartGame.rpc()
 	pass # Replace with function body.
+
+@rpc("any_peer", "call_local", "reliable")
+func StartGame():
+	var scene = load("res://Scenes/test_movement_scene.tscn").instantiate()
+	get_tree().root.add_child(scene)
+	self.hide()
+	
