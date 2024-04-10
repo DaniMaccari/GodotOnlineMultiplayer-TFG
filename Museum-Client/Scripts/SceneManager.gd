b02e0167d08e653902f4e36bd8244a2ef1ee3c42
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-04-10 13:53:39 +0200

    synchro working, camera bug
    both instances render from the same player camera, but each instance controls a different character

--- Museum-Client/Scripts/SceneManager.gd
+++ Museum-Client/Scripts/SceneManager.gd
@@ -6,6 +6,7 @@ func _ready():
 	var index = 0
 	for i in GameManager.Players:
 		var currentPlayer = PlayerScene.instantiate()
+		currentPlayer.name = str(GameManager.Players[i].id)
 		add_child(currentPlayer)
 		for spawn in get_tree().get_nodes_in_group("SpawnLocationPoint"):
 			if spawn.name == str(index):
--- Museum-Client/Scripts/player_movement.gd
+++ Museum-Client/Scripts/player_movement.gd
@@ -12,29 +12,33 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera = $PlayerCamera
 
 func _ready():
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
 	pass
 func _physics_process(delta):
-	# Add the gravity.
 	
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	#is this multiplayer authority (input is from this player)
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
 
 func _input(event):
 
