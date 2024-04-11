8c5cb8669f9421f83d1a082d746d45cf582dae9b
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-04-11 15:14:35 +0200

    security push

--- Museum-Client/Scripts/player_movement.gd
+++ Museum-Client/Scripts/player_movement.gd
@@ -7,6 +7,7 @@ var MOUSE_SENSITIVITY = 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var myID
 
 @export var camera: Camera3D
 #var camera: Camera3D
@@ -19,14 +20,16 @@ func _ready():
 	
 	#????????
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		myID = multiplayer.get_unique_id()
 		camera.current = true
-	#is_local_player = (multiplayer.get_network_master() == multiplayer.get_unique_id())
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
 	
+	print("-", $MultiplayerSynchronizer.get_multiplayer_authority(), " ", multiplayer.get_unique_id())
 	#is this multiplayer authority (input is from this player)
-	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == myID:
 		# Add the gravity.
 		if not is_on_floor():
 			velocity.y -= gravity * delta
@@ -38,6 +41,7 @@ func _physics_process(delta):
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		print(input_dir)
 		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
 			velocity.x = direction.x * SPEED
@@ -49,11 +53,11 @@ func _physics_process(delta):
 		move_and_slide()
 
 func _input(event):
-	if camera == null:
-		return
-	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_ESCAPE:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE
-	elif event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+
+	if Input.is_action_just_pressed("ui_quit"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif event is InputEventMouseMotion:
 		rotate_y(-deg_to_rad(event.relative.x) * MOUSE_SENSITIVITY)
 		camera.rotate_x(-deg_to_rad(event.relative.y) * MOUSE_SENSITIVITY)
-		$CollisionShape3D/eyesPosition.rotate_x(camera.rotation.x)
+		#camera.rotation.x = clamp(camera.rotation.y, -PI/2, -PI/2)
+		$CollisionShape3D/eyesPosition.rotate_x(camera.rotation.y)
