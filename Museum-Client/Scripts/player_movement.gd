379b2064c489dbf82cc39001f8fc6dccddfb0e15
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-04-12 13:38:50 +0200

    handcuffs working fixed

--- Museum-Client/Scripts/player_movement.gd
+++ Museum-Client/Scripts/player_movement.gd
@@ -46,7 +46,6 @@ func _physics_process(delta):
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		print(input_dir)
 		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
 			velocity.x = direction.x * SPEED
@@ -74,9 +73,10 @@ func _input(event):
 @rpc("any_peer")
 func get_handcuffed():
 	isHandcuffed = true
-	
-	if myBody:
-		var material = myBody.material_override
-		material.albedo_color = Color(1, 0, 0)
+	print("Im handcuffed ", myID)
+	#
+	#if myBody:
+		#var material = myBody.material_override
+		#material.albedo_color = Color(1, 0, 0)
 	#$CollisionShape3D/body.get_sur
 	
