061e650b4f7a9dba3a6dd824634f0aebf61ee2f3
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-04-17 22:22:06 +0200

    texture added

--- Museum-Client/Scripts/paint_script.gd
+++ Museum-Client/Scripts/paint_script.gd
@@ -1,16 +1,35 @@
 extends StaticBody3D
 
 var allTextures = [
-#    original                              ,   vadaliced 
+#    original                              ,   vandaliced 
 	["res://paintTextures/paintTest_1a.png", "res://paintTextures/paintTest_1b.png"],
 	["res://paintTextures/paintTest_1a.png", "res://paintTextures/paintTest_1b.png"]
 ]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var frameRate = 10/1
+var lastFrame
 
+func _ready():
+	var random_texture = allTextures[randi() % allTextures.size()]
+	var material = StandardMaterial3D.new()
+	material.albedo_texture = random_texture
+	#set_material(material)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	lastFrame += delta
+	if lastFrame >= frameRate:
+		lastFrame -= frameRate
+		_slow_process(frameRate)
+
+func _slow_process(delta):
+	if multiplayer.get_unique_id() == 1:
+		pass
+
+
+
+
+
+
