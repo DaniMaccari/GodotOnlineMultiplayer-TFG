c734886e99c1795a396ad12ff7ea2fcd45012486
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-04-18 16:20:17 +0200

    change paint exture 4 fixed

--- Museum-Client/Scripts/paint_script.gd
+++ Museum-Client/Scripts/paint_script.gd
@@ -11,17 +11,22 @@ var allTextures = [
 var paintID : int
 var frameRate = 10/1
 var lastFrame
+@onready var notPainted : MeshInstance3D = $CollisionShape3D/pintura0
+@onready var yesPainted : MeshInstance3D = $CollisionShape3D/pintura1
 
 func _ready():
 	paintID = 0 #number added when games start
 	
-	paintTexture.material_override.texture = ResourceLoader.load(allTextures[paintID][0])
+	notPainted.visible = true
+	yesPainted.visible = false
 	#paintTexture.material_override.set_texture(allTextures[paintID][0])
 
 
 func VandalicePainting():
 	print("paint_script -", "PAINTINGG")
-	paintTexture.material_override.texture = ResourceLoader.load(allTextures[paintID][1])
+	notPainted.visible = false
+	yesPainted.visible = true
+	#paintTexture.material_override.texture = ResourceLoader.load(allTextures[paintID][1])
 	#set in the singleton this paint to VANDALICED TRUE
 	pass
 
