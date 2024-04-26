5eb774aafe634111ae50427229e351a43c40552e
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-04-26 15:05:29 +0200

    select badguy dictionary working

--- Museum-Client/Scripts/GameManager.gd
+++ Museum-Client/Scripts/GameManager.gd
@@ -14,16 +14,25 @@ func selectBadGuys():
 	#selec unmber of badguys
 	var numPlayers = Players.size()
 	var numBadGuys = 2
+	var badGuysPos = [-1, -1]
 	if numPlayers <= 5: #if less than 6 players 1 badguy
 		numBadGuys -= 1
 	
 	var rng = RandomNumberGenerator.new()
 	while numBadGuys > 0:
-		var setRandom = rng.randi_range(0, numPlayers)
-		if !Players[setRandom].badguy: #if not already badguy
-			Players[setRandom].badguy = true
+		var setRandom = rng.randi_range(0, numPlayers -1)
+		
+		if badGuysPos[0] == -1:
+			badGuysPos[0] = setRandom
 			numBadGuys -= 1
+		elif badGuysPos[0] != setRandom:
+			badGuysPos[1] = setRandom
+			numBadGuys -=1
 	
+	var playerCounter = 0
 	#send info to all players
 	for player in Players:
-		rpc_id(player.id, "setBadGuy", player.badguy)
+		if playerCounter == badGuysPos[numBadGuys]:
+			Players[player].badguy = true
+			print(Players[player])
+		rpc_id(Players[player].id, "setBadGuy", Players[player].badguy)
